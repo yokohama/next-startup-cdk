6f1957e4ddb26aec607e8139f2cdc99da336950a
yokohama <yuhei.yokohama@gmail.com>
2022-09-23 00:05:30 +0900

    リファクタリング

--- lib/next-startup-stack.ts
+++ lib/next-startup-stack.ts
@@ -2,18 +2,142 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 import { 
+  aws_ecr as ecr,
   aws_ec2 as ec2,
+  aws_rds as rds,
   aws_ecs as ecs,
   aws_ecs_patterns as ecs_patterns,
-  aws_apigateway as apigateway 
+  aws_secretsmanager as secretmanager,
+  aws_apigateway as apigateway
 } from 'aws-cdk-lib'
-import { StackPropsType } from './types/TargetEnvType'
+
+import { StackPropsType } from './types/TargetEnvType';
 
 export class NextStartupStack extends cdk.Stack {
-  //constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  public readonly vpc: ec2.IVpc
+
   constructor(scope: Construct, id: string, props: StackPropsType) {
     super(scope, id, props);
     
+    // ECR
+    const repository = new ecr.Repository(this, 'Ecr', {
+      imageScanOnPush: true,
+    });
+    
+    // VPC
+    this.vpc = new ec2.Vpc(this, 'Vpc', {
+      cidr: props.vpcSubnet,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      natGateways: 1,
+      maxAzs: 2,
+      subnetConfiguration: [
+        { 
+          name: 'Public', subnetType: ec2.SubnetType.PUBLIC, 
+          cidrMask: 27 
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          //subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 27,
+        },
+      ],
+    });
+    
+    // RDS Secret
+    const secret = new secretmanager.Secret(this, 'RdsSecret', {
+      secretName: `postgres-${props.targetEnv}`,
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: props.dbUser }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: 'password'
+      }
+    })
+
+    // RDS SecurityGroup
+    const rdsSG = new ec2.SecurityGroup(this, 'RdsSG', {
+      vpc: this.vpc,
+      allowAllOutbound: true
+    });
+    rdsSG.addIngressRule(
+      ec2.Peer.ipv4('0.0.0.0/0'),
+      ec2.Port.tcp(5432)
+    );
+
+    const postgresql = new rds.DatabaseInstance(this, 'Rds', {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_14_2,
+      }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+      vpc: this.vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        //subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+      },
+      securityGroups: [rdsSG],
+      credentials: rds.Credentials.fromSecret(secret),
+      databaseName: props.dbName
+    });
+    
+    // ECS Cluster
+    const ecsCluster = new ecs.Cluster(this, 'EcsCluster', {
+      vpc: this.vpc,
+      containerInsights: true,
+    });
+    
+    // ECS Task Definition
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition')
+
+    taskDefinition.addContainer('Container', {
+      image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
+      memoryLimitMiB: 256,
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: repository.repositoryName,
+      }),
+      environment: {
+        RAILS_ENV: props.railsEnv,
+        RAILS_LOG_TO_STDOUT: 'true',
+        DATABASE_HOST: postgresql.instanceEndpoint.hostname,
+        DATABASE_NAME: props.dbName,
+        DATABASE_USER: props.dbUser,
+      },
+      secrets: {
+        'DATABASE_PASSWORD': ecs.Secret.fromSecretsManager(secret, 'password')
+      },
+      //entryPoint: ['rake', 'db:migrate'],
+      //workingDirectory: '/opt/app'
+    })
+    .addPortMappings({
+      protocol: ecs.Protocol.TCP,
+      containerPort: 3000
+    });
+    
+    const loadBalancedFargateService = 
+      new ecs_patterns.NetworkLoadBalancedFargateService(
+        this, 
+        'LoadBalancedFargateService', 
+        {
+          assignPublicIp: false,
+          cluster: ecsCluster,
+          taskSubnets: this.vpc.selectSubnets({
+            //subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+          }),
+          memoryLimitMiB: 1024,
+          cpu: 512,
+          desiredCount: 2,
+          taskDefinition: taskDefinition,
+          publicLoadBalancer: true,
+        }
+      )
+
+    loadBalancedFargateService.service.connections.allowFrom(
+      ec2.Peer.ipv4(this.vpc.vpcCidrBlock),
+      ec2.Port.tcp(3000)
+    )
+
     // Auto Scaling Settings
     /*
     const scalableTarget =
@@ -53,7 +177,6 @@ export class NextStartupStack extends cdk.Stack {
       },
     });
     
-    // API Gateway
     const api = new apigateway.RestApi(this, 'next-startup-api', {
       restApiName: `NextStartUpApi-${props.targetEnv}`
     })
--- lib/types/TargetEnvType.ts
+++ lib/types/TargetEnvType.ts
@@ -7,6 +7,13 @@ export type RailsEnvType = 'development' | 'production'
 export type StackPropsType = StackProps & {
   targetEnv: TargetEnvType, 
   railsEnv: RailsEnvType, 
+  vpcSubnet: string,
   dbName: string, 
   dbUser: string
+}
+
+export const VpcSubnet: { [key: string]: string } = {
+  local: '10.0.0.0/24',
+  dev: '10.0.0.1/24',
+  prod: '10.0.0.2/24'
 }
